bdec38c1712e51476f516f609000b264ea87ce24
Paulo Mariano <paulomariano2501@gmail.com>
2023-01-20 00:51:24 -0300

    enable cors

--- backend/src/auth/models/LoginRequestBody.ts
+++ backend/src/auth/models/LoginRequestBody.ts
@@ -1,9 +1,5 @@
-import { IsEmail, IsString } from 'class-validator';
-
 export class LoginRequestBody {
-  @IsEmail()
   email: string;
 
-  @IsString()
   password: string;
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 3333;
+
+  app.enableCors()
+
   await app.listen(port);
 
   console.log(`Running on port ${port}`);
